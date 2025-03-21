9d6a009318eacdb21426d2301c034cdee5787614
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-21 15:19:07 +0100

    [UPD]Implement function to reverse a string for palindrome check

--- js/script.js
+++ js/script.js
@@ -9,4 +9,17 @@
 let parola = prompt('Inserisci la parola');
 console.log(parola);
 //creo la funzione per capire se la parola inserita è palindroma
+function polindromWord(parola){
+    //creo una variabile per salvare la parola al contrario
+    let parolaReverse = "";
+    //creo un ciclo per invertire la parola inserita prendendo ogni lettera dalla fine
+    //let i = parola.length - 1; i >= 0 ; i-- significa che i parte dalla lunghezza della parola - 1, quindi dall'ultima lettera
+    for (let i = parola.length - 1; i >= 0 ; i--){
+        //let i = parola.length - 1; i >= 0 ; i-- significa che i parte dalla lunghezza della parola - 1, quindi dall'ultima lettera
+        //parolaReverse += parola[i]; significa che prendo la lettera i della parola e la metto in parolaReverse e questo fino alla fine della lunghezza della parola
+        parolaReverse += parola[i];
+    }
+    //ritorno la parola al contrario
+    return parolaReverse;
+}
 
