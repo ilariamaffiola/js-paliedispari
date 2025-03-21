d48dcd42a26d49f9b57be61582939ab12abe67d9
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-21 15:18:47 +0100

    [UPD]Add initial implementation for palindrome check functionality

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Palindroma 
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+//significato di palindromo:
+//palindromo è quando una parola è uguale se letta da destra a sinistra e viceversa
+//esempio: anno, otto, radar, osso, radar, ossesso, inni, onorarono, ecc.
+
+//chiedo all'utente di inserire una parola
+let parola = prompt('Inserisci la parola');
+console.log(parola);
+//creo la funzione per capire se la parola inserita è palindroma
+
